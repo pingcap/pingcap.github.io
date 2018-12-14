42e4e9d86073ff60620b1ad06f21772d1e35c610
YiniXu9506 <xuyini@pingcap.com>
2018-12-14 14:18:27 +0800

    update instructor avatars and sponsor logos

--- src/js/devcon.js
+++ src/js/devcon.js
@@ -1,6 +1,6 @@
 // handle wethear display website navbar
 function handleWebsiteNavDisplay() {
-  console.log('scrolling...')
+  // console.log('scrolling...')
   var scrollVal = $(this).scrollTop(),
     h = $('header').height()
 
@@ -28,7 +28,12 @@ function smoothScroll() {
     case 'contact-btn':
       sectionName = '.contact__container h1'
       break
+    case 'signup-btn':
+      sectionName = '.signup__container'
+      break
   }
+
+  console.log('$(sectionName).offset().top', $(sectionName).offset().top)
   var extraH = 0
   if ($('header').length && !$('header').is(':hidden')) {
     extraH = 2 * $('header').height()
@@ -44,176 +49,37 @@ function smoothScroll() {
 }
 
 function calcBtnPosition() {
-  console.log('hei')
+  // console.log('hei')
   if (window.matchMedia('(max-width: 1100px)').matches) {
-    console.log('mobile')
-    $('.banner button').css('left', 'auto')
-    $('.banner button').css('top', 'auto')
+    // console.log('mobile')
+    // $('.banner button').css('left', 'auto')
+    // $('.banner button').css('top', 'auto')
     $('.banner .image img').attr(
       'src',
       '/images/community/devcon-banner-mobile.png'
     )
-    console.log('height:', $('.banner, .image img').height())
-    // var wd = $('.banner, .image img').width() * 0.127 // 74 * W / 1368
-    var hd = $('.banner, .image img').height() * 0.734 // 781* H / 573
-    // var btnTop = hd + 30
-    // //
-    // $('.banner p').css('left', wd)
-    // $('.banner p').css('top', hd)
-    $('.banner button').css('right', '1rem')
-    $('.banner button').css('top', hd)
+    // console.log('height:', $('.banner, .image img').height())
+    // var hd = $('.banner, .image img').height() * 0.734 // 781* H / 573
+    // $('.banner button').css('right', '1rem')
+    // $('.banner button').css('top', hd)
   } else {
-    console.log('pc')
+    // console.log('pc')
     $('.banner .image img').attr(
       'src',
       '/images/community/devcon-banner-pc.png'
     )
-    var wd = $('.banner, .image img').width() * 0.127 // 74 * W / 1368
-    var hd = $('.banner, .image img').height() * 0.689 // 424* H / 615
-    var btnTop = hd + 30
-    // //
-    // $('.banner p').css('left', wd)
-    // $('.banner p').css('top', hd)
-    $('.banner button').css('left', wd)
-    $('.banner button').css('top', btnTop)
-  }
+    // var wd = $('.banner, .image img').width() * 0.127 // 74 * W / 1368
+    // var hd = $('.banner, .image img').height() * 0.689 // 424* H / 615
+    // var btnTop = hd + 30
 
-  // console.log('h: ', $('.banner, .image img').height())
-  // console.log('w:', $('.banner, .image img').width())
-  // var btnTop = hd + 30
-  // // //
-  // // $('.banner p').css('left', wd)
-  // // $('.banner p').css('top', hd)
-  // $('.banner button').css('left', wd)
-  // $('.banner button').css('top', btnTop)
+    // $('.banner button').css('left', wd)
+    // $('.banner button').css('top', btnTop)
+  }
 }
-// calculate the positions of address and button in banner
-// function calcBtnPosition() {
-//   // console.log('titleImg width: ', titleImg_W, titleImg_Top)
-//   console.log('imge height and width: ', $('.banner .banner__content').height())
-//   console.log('width: ', $('.banner .banner__content').width())
-//   if (window.matchMedia('(max-width: 599px)').matches) {
-//     $('.banner .image img').attr(
-//       'src',
-//       '/images/community/devcon-banner-mobile.png'
-//     )
-//     console.log('yes matched 1100px')
-//     var logo_W = $('.banner, .image').width() * 17 / 100
-//     var logo_L = 0
-//     var logo_top = logo_W / 2
-//     // console.log(
-//     //   'imge height and width: ',
-//     //   $('.banner .banner__content').height()
-//     // )
-//     // console.log('width: ', $('.banner .banner__content').width())
-//     var titleImg_W = $('.banner, .image').width() * 750 / 1366
-//     var titleImg_Top = -$('.banner, .image').width() * 50 / 1500
-//   } else if (
-//     window.matchMedia('(max-width: 1099px)').matches &&
-//     window.matchMedia('(min-width: 600px)').matches
-//   ) {
-//     $('.banner .image img').attr(
-//       'src',
-//       '/images/community/devcon-banner-mobile.png'
-//     )
-//     console.log('yes matched 1100px')
-//     var logo_W = $('.banner, .image').width() * 15 / 100
-//     var logo_L = 0
-//     var logo_top = logo_W / 2
-//     // console.log(
-//     //   'imge height and width: ',
-//     //   $('.banner .banner__content').height()
-//     // )
-//     // console.log('width: ', $('.banner .banner__content').width())
-//     var titleImg_W = $('.banner, .image').width() * 750 / 1366
-//     var titleImg_Top = -$('.banner, .image').width() * 50 / 1500
-//   } else if (
-//     window.matchMedia('(max-width: 1399px)').matches &&
-//     window.matchMedia('(min-width: 1100px)').matches
-//   ) {
-//     $('.banner .image img').attr(
-//       'src',
-//       '/images/community/devcon-banner-pc.svg'
-//     )
-//     console.log('yes matched 1099 - 1400')
-//     // $('.banner .image img').attr(
-//     //   'src',
-//     //   '/images/community/devcon-banner-pc.svg'
-//     // )
-//     // console.log(
-//     //   'imge height and width: ',
-//     //   $('.banner .banner__content').height()
-//     // )
-//     // console.log('width: ', $('.banner .banner__content').width())
-//     var logo_W = $('.banner, .image').width() * 10 / 100
-//     var logo_L = 0
-//     var logo_top = logo_W / 3
-//     var titleImg_W = $('.banner, .image').width() * 600 / 1366
-//     var titleImg_Top = -$('.banner, .image').height() * 100 / 1400
-//   } else if (
-//     window.matchMedia('(max-width: 1599px)').matches &&
-//     window.matchMedia('(min-width: 1400px)').matches
-//   ) {
-//     $('.banner .image img').attr(
-//       'src',
-//       '/images/community/devcon-banner-pc.svg'
-//     )
-//     console.log('yes matched 1400 - 1599')
-//     var logo_W = $('.banner, .image').width() * 10 / 100
-//     var logo_L = 0
-//     var logo_top = logo_W / 3
-//     var titleImg_W = $('.banner, .image').width() * 450 / 1366
-//     var titleImg_W = $('.banner, .image').width() * 550 / 1366
-//     var titleImg_Top = -$('.banner, .image').height() * 100 / 1400
-//     window.matchMedia('(max-width: 1400px)').matches &&
-//       window.matchMedia('(min-width: 1099px)').matches
-//   } else if (
-//     // window.matchMedia('(max-width: 2000px)').matches &&
-//     window.matchMedia('(min-width: 1600px)').matches
-//   ) {
-//     $('.banner .image img').attr(
-//       'src',
-//       '/images/community/devcon-banner-pc.svg'
-//     )
-//     console.log('yes matched 1600 - 2000')
-//     var logo_W = $('.banner, .image').width() * 10 / 100
-//     var logo_L = -logo_W
-//     var logo_top = logo_W / 3
-//     var titleImg_W = $('.banner, .image').width() * 450 / 1366
-//     // var titleImg_W = $('.banner__content,.banner-title').width() * 33 / 100
-//     var titleImg_Top = -$('.banner, .image').height() * 100 / 1400
-//     window.matchMedia('(max-width: 1400px)').matches &&
-//       window.matchMedia('(min-width: 1099px)').matches
-//   }
-//   console.log('left: ', logo_L)
-//   console.log(
-//     'wd and top: ',
-//     titleImg_W,
-//     titleImg_Top,
-//     logo_L,
-//     logo_top,
-//     logo_W
-//   )
-//   // var address_H = $('.banner__content .banner-title')
-//   $('.banner__content .banner-tidb-logo').css('width', logo_W)
-//   $('.banner__content .banner-tidb-logo').css('left', logo_L)
-//   $('.banner__content .banner-tidb-logo').css('top', logo_top)
-//   $('.banner__content .banner-title').css('width', titleImg_W)
-//   $('.banner__content .banner-title').css('top', titleImg_Top)
-//   // $('.banner__content .banner-address').css('top', titleImg_Top)
-//   // var wd = $('.banner, .img').width() * 229 / 1267
-//   // var hd = $('.banner, .img').height() * 354 / 570
-//   // var btnTop = hd + 60
-//   // //
-//   // $('.banner p').css('left', wd)
-//   // $('.banner p').css('top', hd)
-//   // $('.banner button').css('left', wd)
-//   // $('.banner button').css('top', btnTop)
-// }
 
 // handle positions of devcon navbar and website navbar when resize window
 function handleWindowResize() {
-  console.log('window size changed')
+  // console.log('window size changed')
   if ($('header').length && !$('header').is(':hidden')) {
     var header_H = $('header').height()
     $('.devcon-nav').css('top', header_H)
@@ -233,22 +99,7 @@ $(document).ready(function() {
     $('.devCon').css('padding-top', 2 * header_H)
   }
 
-  // if (window.matchMedia('(max-width: 1100px)').matches) {
-  //   console.log('m')
-  //   $('.banner .image img').attr(
-  //     'src',
-  //     '/images/community/devcon-banner-mobile.png'
-  //   )
-  //   calcBtnPosition()
-  // } else {
-  //   console.log('p')
-  //   $('.banner .image img').attr(
-  //     'src',
-  //     '/images/community/devcon-banner-pc.png'
-  //   )
-  //   console.log($('.banner .image img').height())
   calcBtnPosition()
-  // }
 
   // handle window scrolls
   $(window).scroll(handleWebsiteNavDisplay)
@@ -263,4 +114,6 @@ $(document).ready(function() {
   $('.instructor-btn').click(smoothScroll)
 
   $('.contact-btn').click(smoothScroll)
+
+  $('.signup-btn').click(smoothScroll)
 })
