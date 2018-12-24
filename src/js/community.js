2c5ff9053c9fb22fbaf930ce02ee2ada9f20fd2a
YiniXu9506 <xuyini@pingcap.com>
2018-12-24 17:32:25 +0800

    update community activities
    Former-commit-id: fe54c8a701be9d850909e9cd6dfb96038e74742d
    Former-commit-id: 9fed2f0d9241a57ab19c132fe7dfeeeeadde46f8

--- src/js/community.js
+++ src/js/community.js
@@ -6,8 +6,54 @@ function handleWindowResize() {
 }
 
 $(document).ready(function() {
+  var calendars = {}
+  var thisMonth = moment().format('YYYY-MM')
+  // Events to load into calendar
+  var eventArray = [
+    {
+      title: 'Multi-Day Event',
+      endDate: thisMonth + '-14',
+      startDate: thisMonth + '-10',
+    },
+    {
+      endDate: thisMonth + '-23',
+      startDate: thisMonth + '-21',
+      title: 'Another Multi-Day Event',
+    },
+    {
+      date: thisMonth + '-27',
+      title: 'Single Day Event',
+    },
+  ]
   // set banner height
   // var BW = $('.banner__container').width()
   // $('.banner__container').css('height', BW / 2.67)
   // $(window).resize(handleWindowResize)
+  calendars.clndr2 = $('.cal2').clndr({
+    lengthOfTime: {
+      days: 14,
+      interval: 7,
+    },
+    events: eventArray,
+    multiDayEvents: {
+      singleDay: 'date',
+      endDate: 'endDate',
+      startDate: 'startDate',
+    },
+    template: $('#template-calendar').html(),
+    clickEvents: {
+      click: function(target) {
+        console.log('Cal-2 clicked: ', target)
+      },
+      nextInterval: function() {
+        console.log('Cal-2 next interval')
+      },
+      previousInterval: function() {
+        console.log('Cal-2 previous interval')
+      },
+      onIntervalChange: function() {
+        console.log('Cal-2 interval changed')
+      },
+    },
+  })
 })
