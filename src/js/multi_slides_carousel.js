dfa43c25d599899b20d80c7d502f9a3166dcc00b
YiniXu9506 <xuyini@pingcap.com>
2019-03-13 20:57:31 +0800

    update hackathon2018
    Former-commit-id: 49bef864454872ff700780ce040161401a7465f8
    Former-commit-id: 6621931d8d9af49e8199a26dc914387b42b2415e

--- src/js/multi_slides_carousel.js
+++ src/js/multi_slides_carousel.js
@@ -1,20 +1,43 @@
 $(document).ready(function() {
-  var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    slidesPerView: 3,
-    slidesPerGroup: 3,
-    spaceBetween: 20,
-    // slidesOffsetAfter: '100',
-    loop: true,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next',
-  })
+  if (window.matchMedia('(max-width: 600px)').matches) {
+    var mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 20,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
+  } else {
+    var mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
+  }
 })
