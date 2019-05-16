7cf81cefc7318473a16baaeed0199ad4277b7a09
YiniXu9506 <xuyini@pingcap.com>
2019-05-16 17:09:02 +0800

    keep the structure of docs in the repo exactly the same
    Former-commit-id: 389c3036122e237deeb471c68f0a9bb5f3e5b765
    Former-commit-id: bfcba5b27f8e88401ecfdd4684cea61034e14618

--- scripts/gen-content.sh
+++ scripts/gen-content.sh
@@ -1,4 +1,5 @@
-node scripts/gen-nav.js 'content/docs/TOC.md' 'data/docs_toc.json' '/docs/'
+node scripts/gen-nav.js 'content/docs/dev/TOC.md' 'data/docs_dev_toc.json' '/docs/'
+node scripts/gen-nav.js 'content/docs/v3.0/TOC.md' 'data/docs_v3.0_toc.json' '/docs/' 
 node scripts/gen-nav.js 'content/docs-cn/TOC.md' 'data/docs_cn_toc.json' '/docs-cn/'
 node scripts/gen-nav.js 'content/about-cn/recruit/TOC.md' 'data/recruit_cn_toc.json' '/about-cn/recruit/'
 node scripts/gen-nav.js 'content/blog-cn/TOC-User-Case.md' 'data/cases_cn_toc.json' '/cases-cn/'
