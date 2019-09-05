0bbcd554b12b99e18b885890b67318e3e3c8a27d
Yue Yang <g1enyy0ung@gmail.com>
2019-09-05 17:50:26 +0800

    Apply anchor operation to all submodules' single.html

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -1,11 +1,12 @@
 ;(function() {
   $('.content.markdown-body')
-    .children('h1, h2, h3, h4, h5, h6')
+    .find('h1, h2, h3, h4, h5, h6')
     .each(function() {
       var that = $(this)
       var newId = that
         .text()
         .replace(/\s/g, '-')
+        .replace(/[\.:：,，]/g, '')
         .toLowerCase()
       that.attr('id', newId)
     })
